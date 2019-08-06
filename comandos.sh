b5c90eceef454676b1c0917c7cf304170a2e893b
carlosrcjuvi <53578203+carlosrcjuvi@users.noreply.github.com>
2019-08-06 17:10:33 -0300

    Add files via upload

---
+++ comandos.sh
@@ -0,0 +1,82 @@
+    1  cd/
+    2  ls
+    3  cd/
+    4  cd
+    5  cd/
+    6  ls
+    7  ls - lh
+    8  ls lh
+    9  cd /
+   10  ls
+   11  cd -
+   12  PWD
+   13  pwd
+   14  mkdir linuxmint
+   15  ls
+   16  cd /linuxmint
+   17  cd /
+   18  pwd
+   19  linuxmint]
+   20  linuxmint
+   21  cd -
+   22  ls
+   23  cd linuxmint/
+   24  ls
+   25  sudo apt
+   26  sudo apt update && sudo apt install git
+   27  git clone https://github.com/carlosrcjuvi/Linux-Mint.git
+   28  ls -lh
+   29  cd linuxmint/
+   30  ls -lh
+   31  echo
+   32  echo Perfect KO
+   33  echo SENAC Tatuapé aprendendo a tatuar o pé 
+   34  echo SENAC Tatuapé aprendendo a tatuar o pé > Cipriano.sh
+   35  ls
+   36  ls -lh
+   37  touch senac.sh
+   38  ls
+   39  ls -lh
+   40  echo Pirocoptero de boné > senac.sh
+   41  ls
+   42  ls -lh
+   43  ls
+   44  ls -lh
+   45  ls
+   46  git clone https://github.com/carlosrcjuvi/Linux-Mint.git
+   47  ls
+   48  mv Cipriano.sh senac.sh Linux-Mint/
+   49  ls
+   50  cd Linux-Mint/
+   51  ls
+   52  ls -lh
+   53  cat LICENSE 
+   54  head
+   55  cd /
+   56  ls
+   57  cd Linuxmint | ls
+   58  cd usr
+   59  ls
+   60  cd usr
+   61  cd local/
+   62  ls
+   63  head LICENSE
+   64  ls
+   65  cd Linux-Mint/
+   66  ls
+   67  head -n20 LICENSE
+   68  Cat n
+   69  tail LICENSE
+   70  tail -n20 LICENSE
+   71  less LICENSE
+   72  cat -n LICENSE | tail
+   73  cat -n200 LICENSE | head -n100 | tail -n200
+   74  head -n100 LICENSE | tail -n200 
+   75  head -n100 LICENSE | tail -n200 | cat -n200
+   76  head -n100 LICENSE | tail -n200 | cat -n100
+   77  cat
+   78  cat -n200 LICENSE | head -200 | tail -n101
+   79  cat -n LICENSE | head -200 | tail -n101
+   80  history
+   81  /cd linuxmint/
+   82  history> comandos.sh
